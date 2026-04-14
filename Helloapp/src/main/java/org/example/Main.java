25adb6d951a8bf1c7bb8318bdb077c717b5719d2
Sanjay7906 <nsbsanjay2006@gmail.com>
2026-04-14 20:12:44 +0530

    Added EmployeeBonus program

--- Helloapp/src/main/java/org/example/Main.java
+++ Helloapp/src/main/java/org/example/Main.java
@@ -1,23 +1,33 @@
 import java.util.Scanner;
 
-class Main
-{
+class EmployeeBonus {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter number: ");
-        int number = input.nextInt();
+        double[] salary = new double[10];
+        double[] years = new double[10];
+        double totalBonus = 0;
 
-        int[] table = new int[10];
+        for (int i = 0; i < 10; i++) {
 
-        for (int i = 0; i < table.length; i++) {
-            table[i] = number * (i + 1);
+            System.out.print("Salary: ");
+            salary[i] = input.nextDouble();
+
+            System.out.print("Years: ");
+            years[i] = input.nextDouble();
+
+            if (salary[i] <= 0 || years[i] < 0) {
+                System.err.println("Invalid input");
+                i--;
+                continue;
+            }
+
+            double bonus = (years[i] > 5) ? salary[i] * 0.05 : salary[i] * 0.02;
+            totalBonus += bonus;
         }
 
-        for (int i = 0; i < table.length; i++) {
-            System.out.println(number + " * " + (i + 1) + " = " + table[i]);
-        }
+        System.out.println("Total Bonus: " + totalBonus);
 
         input.close();
     }
