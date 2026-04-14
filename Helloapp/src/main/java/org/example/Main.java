bced10dc0bc2c1a4e57e1ded88766a8d0e677cb2
Sanjay7906 <nsbsanjay2006@gmail.com>
2026-04-14 19:31:48 +0530

    Add Level 2: Rocket countdown using while loop

--- Helloapp/src/main/java/org/example/Main.java
+++ Helloapp/src/main/java/org/example/Main.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = input.nextInt();
+        System.out.print("Enter countdown start: ");
+        int counter = input.nextInt();
 
-        // Boolean expression for divisibility
-        boolean isDivisible = (number % 5 == 0);
-        System.out.println("Is the number " + number + " divisible by 5? " + isDivisible);
+        // While loop for countdown
+        while (counter >= 1) {
+            System.out.println(counter + "...");
+            counter--; // Decrement
+        }
+        System.out.println("Liftoff!");
         input.close();
     }
 }
