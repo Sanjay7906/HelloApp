858ba6005229051caad7f1d8f48af18eb716b82e
Sanjay7906 <nsbsanjay2006@gmail.com>
2026-04-14 19:42:36 +0530

    Added FizzBuzz program

--- Helloapp/src/main/java/org/example/Main.java
+++ Helloapp/src/main/java/org/example/Main.java
@@ -5,14 +5,17 @@ class Main {
         Scanner input = new Scanner(System.in);
 
         int number = input.nextInt();
-        int sum = 0;
 
-        while (number != 0) {
-            sum += number % 10;
-            number /= 10;
+        for (int i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println("FizzBuzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
         }
-
-        System.out.println("Sum = " + sum);
 
         input.close();
     }
