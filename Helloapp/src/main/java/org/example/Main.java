d68bcbcd78d47b670c22e49523ca70691d943f4d
Sanjay7906 <nsbsanjay2006@gmail.com>
2026-04-14 19:43:34 +0530

    Added Factors program

--- Helloapp/src/main/java/org/example/Main.java
+++ Helloapp/src/main/java/org/example/Main.java
@@ -7,13 +7,7 @@ class Main {
         int number = input.nextInt();
 
         for (int i = 1; i <= number; i++) {
-            if (i % 3 == 0 && i % 5 == 0)
-                System.out.println("FizzBuzz");
-            else if (i % 3 == 0)
-                System.out.println("Fizz");
-            else if (i % 5 == 0)
-                System.out.println("Buzz");
-            else
+            if (number % i == 0)
                 System.out.println(i);
         }
 
